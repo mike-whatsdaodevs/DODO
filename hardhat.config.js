2294f5280bc88dbfef5f9d3571214307714bed64
mike <mike@whatsdao.org>
2024-04-20 19:47:58 +0800

    construct dodo code

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,83 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle')
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.24",
-};
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.16",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            }
+        },
+      },
+      {
+        version: "0.8.24",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            }
+        },
+      },
+      {
+        version: "0.6.10",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            }
+        },
+      },
+    ]
+  },
+  networks: {
+    goerli: {
+      url: process.env.GOERLIURL,
+      accounts: 
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    local: {
+      url: process.env.LOCAL,
+      accounts: 
+        process.env.PRIVATE_KEY_LOCAL !== undefined ? [process.env.PRIVATE_KEY_LOCAL] : [],
+    }
+  },
+  mocha: {
+    timeout: 40000,
+  },
+  etherscan: {
+    apiKey: {
+      btcc: "Jo4nzq1PZTlknkfLht960WfC3J4jMKKX"
+    },
+    customChains: [
+      {
+        network: "btcc",
+        chainId: 963,
+        urls: {
+          apiURL: "https://scan.bitcoincode.technology/api",
+          browserURL: "https://scan.bitcoincode.technology"
+        }
+      }
+    ]
+  }
+
+}
